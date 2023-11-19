edeed3f0a9cc58fb55762c4ccfd61e2ef4058413
visions <44182242+visions4k@users.noreply.github.com>
2023-11-19 02:35:25 -0500

    Update cload.py

--- checker/cload.py
+++ checker/cload.py
@@ -5,6 +5,10 @@ with open('config/config.json', 'r') as f:
 
 robloxUserID = data['Config']['robloxUserID']
 robloxCookie =  data['Config']['robloxCookie']
+proxyEnabled = data['Checker']['Proxy']['isEnabled']
+autoScrape = data['Checker']['Proxy']['autoScrape']
+timeoutC = data['Checker']['Proxy']['timeout']
 antiRatelimit = data['Checker']['antiRatelimit']
 discordEnabled = data['Discord']['isEnabled']
 webhookURL = data['Discord']['webhookURL']
+proxyList = []
