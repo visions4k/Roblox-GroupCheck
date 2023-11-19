7fd191ee4ed3adb596cf69589c9b17dfdcdc1126
visions <44182242+visions4k@users.noreply.github.com>
2023-11-19 02:38:23 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,8 +1,16 @@
 import asyncio
 from checker.process import Process
+from checker.bypass import Bypass
+from checker.cload import *
 
 async def start():
-    process = Process()
+    if proxyEnabled:
+        beforeCookie = robloxCookie
+        bypass = Bypass(beforeCookie)
+        bypassedCookie = bypass.start_process()
+        process = Process(bypassedCookie)
+    else:
+        process = Process(robloxCookie)
     await process.processGroups()
 
 if __name__ == "__main__":
