38ec1005217966b6df8182fde26c5b02581e201a
visions <44182242+visions4k@users.noreply.github.com>
2023-11-18 22:31:34 -0500

    Update cload.py

--- checker/cload.py
+++ checker/cload.py
@@ -4,9 +4,7 @@ with open('config/config.json', 'r') as f:
     data = json.load(f)
 
 robloxUserID = data['Config']['robloxUserID']
-robloxCookie = {
-    ".ROBLOSECURITY": data['Config']['robloxCookie']
-}
+robloxCookie =  data['Config']['robloxCookie']
 antiRatelimit = data['Checker']['antiRatelimit']
 discordEnabled = data['Discord']['isEnabled']
 webhookURL = data['Discord']['webhookURL']
