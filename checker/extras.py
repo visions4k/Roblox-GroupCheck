9619b8a09ef7a8d50736db298c4ccb53fff4ecba
visions <44182242+visions4k@users.noreply.github.com>
2023-11-19 02:35:40 -0500

    Update extras.py

--- checker/extras.py
+++ checker/extras.py
@@ -4,11 +4,37 @@ from checker.cload import *
 from bs4 import BeautifulSoup
 from colorama import Fore, Style
 
-def successful(input):
-    print(f"{Fore.GREEN}✓ {input}{Style.RESET_ALL}")
     
-def checking(input):
+psUrl = [  
+    'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=1000&country=all&ssl=all&anonymity=all',      
+]
+
+def loadProxies():
+    if not autoScrape:
+        with open('config/proxies.txt') as f:
+            proxyList.extend(line.strip() for line in f if line.strip())
+    else:
+        proxyList.clear()
+        for url in psUrl:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            sproxies = {
+                line.strip()
+                for line in soup.get_text().splitlines()
+                if line.strip()
+            }
+            ndproxies = [proxy for proxy in sproxies if proxy not in proxyList]
+            proxyList.extend(ndproxies)
+    return proxyList
+
+def checkedG(input):
+    print(f"{Fore.BLUE}★ {input}{Style.RESET_ALL}")
+    
+def statG(input):
     print(f"{Fore.YELLOW}► {input}{Style.RESET_ALL}")
+    
+def doneG(input):
+    print(f"{Fore.GREEN}► {input}{Style.RESET_ALL}")
     
     
 async def sendWebhook(gid, name, members, clothing, funds, fundsPending, games, visits):
